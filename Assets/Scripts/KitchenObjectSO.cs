77e36da7e6322601cb0ce84f2bf711a4ff21ac5a
Andrei <ionascuandrei320@gmail.com>
2024-08-31 16:54:24 +0300

    Initial commit

---
+++ Assets/Scripts/KitchenObjectSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//this is a scriptable object, used to handle all the different types of ingredients
+[CreateAssetMenu()]
+public class KitchenObjectSO : ScriptableObject
+{
+    public Transform prefab;
+    public Sprite sprite;
+    public string objectName;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# KitchenChaos-Tutorial
+Following Code Monkey's tutorial on 3D game development in Unity
