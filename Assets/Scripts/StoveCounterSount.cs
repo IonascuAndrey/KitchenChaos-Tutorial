6995fc47ccceadcad7142ac88aa41c7190e43b3c
Andrei <ionascuandrei320@gmail.com>
2024-09-15 14:58:13 +0300

    Final SinglePlayer Version

--- Assets/Scripts/StoveCounterSount.cs
+++ Assets/Scripts/StoveCounterSount.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class StoveCounterSount : MonoBehaviour
 {
-    private AudioSource audioSource;
     [SerializeField] private StoveCounter stoveCounter;
+
+    private AudioSource audioSource;
+    private float warningSoundTimer;
+    private bool playWarningSound;
+
 
     private void Awake() {
         audioSource = GetComponent<AudioSource>();
@@ -13,6 +17,12 @@ public class StoveCounterSount : MonoBehaviour
 
     private void Start() {
         stoveCounter.OnStateChanged += StoveCounter_OnStateChanged;
+        stoveCounter.OnProgressChanged += StoveCounter_OnProgressChanged;
+    }
+
+    private void StoveCounter_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e) {
+        float burnShowProgressAmount = .5f;
+        playWarningSound = stoveCounter.IsFried() && e.progressNormalized >= burnShowProgressAmount;
     }
 
     private void StoveCounter_OnStateChanged(object sender, StoveCounter.OnStateChangedEventArgs e) {
@@ -21,6 +31,18 @@ public class StoveCounterSount : MonoBehaviour
             audioSource.Play();
         } else {
             audioSource.Pause();
+        }
+    }
+
+    private void Update() {
+        if (playWarningSound) {
+            warningSoundTimer -=Time.deltaTime;
+            if (warningSoundTimer <= 0f) {
+                float warningSoundtimerMax = .2f;
+                warningSoundTimer = warningSoundtimerMax;
+
+                SoundManager.Instance.PlayWarningSound(stoveCounter.transform.position);
+            }
         }
     }
 }
